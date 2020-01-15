4473447d542413fa0a26fa1df16874510466b654
Justin Phillips <justinphil02@yahoo.com>
2020-01-15 01:11:22 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,15 +1,15 @@
-class PigLatinizer 
+class PigLatinizer
   attr_accessor :user_phrase
 
   def initialize
-    
-  end 
+
+  end
 
   def piglatinize(input)
     if input.scan(/^[aeiou]/)
       input + "way"
     else input.scan(/^[^aeiou]{2}/)
       input + "ay"
-    end 
-  end 
+    end
+  end
 end 
