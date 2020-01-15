4f9894a84b4e423a464cf49551610ad9bfe188b6
Justin Phillips <justinphil02@yahoo.com>
2020-01-15 01:25:19 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -4,12 +4,13 @@ class PigLatinizer
     if input.match?(/^[aeiouAEIOU]/)
       input + "way"
     elsif input.match?(/^[^aeiouAEIOU]{3}/)
+      array = input.split(//)
       new_word = array[1..-1].join + array[0] + "ay"
     elsif input.match?(/^[^aeiouAEIOU]{2}/)
+      array = input.split(//)
       new_word = array[1..-1].join + array[0] + "ay"
     elsif input.match?(/^[^aeiouAEIOU]/)
       array = input.split(//)
-    #  binding.pry
       new_word = array[1..-1].join + array[0] + "ay"
     end
   end
