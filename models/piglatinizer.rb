516b960b41d0e0dcc4ec2db436e7f70c983d179e
Justin Phillips <justinphil02@yahoo.com>
2020-01-15 01:25:55 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -5,10 +5,10 @@ class PigLatinizer
       input + "way"
     elsif input.match?(/^[^aeiouAEIOU]{3}/)
       array = input.split(//)
-      new_word = array[1..-1].join + array[0] + "ay"
+      new_word = array[3..-1].join + array[0] + "ay"
     elsif input.match?(/^[^aeiouAEIOU]{2}/)
       array = input.split(//)
-      new_word = array[1..-1].join + array[0] + "ay"
+      new_word = array[2..-1].join + array[0] + "ay"
     elsif input.match?(/^[^aeiouAEIOU]/)
       array = input.split(//)
       new_word = array[1..-1].join + array[0] + "ay"
